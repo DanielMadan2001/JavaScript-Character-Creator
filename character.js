7ce4d6780a65975b92d05c1450c311fb4f391ef9
Daniel Madan <61928543+DanielMadan2001@users.noreply.github.com>
2022-01-22 23:05:56 -0500

    Started

---
+++ character.js
@@ -0,0 +1,25 @@
+class Character {
+    constructor ()
+    {
+        this.name = "";
+        this.level = 0;
+        this.age = 20;
+        // this.class = 
+        this.stats = {
+            hp: 0,
+            mp: 0,
+            st: 0,
+
+            strength: 0,
+            durability: 0,
+            intellect: 0,
+            resistance: 0,
+            agility: 0,
+            dexterity: 0,
+            luck: 0,
+            greed: 0
+        };
+        // randomAll() {}
+    }
+}
+export default Character;
---
+++ script.js
@@ -0,0 +1,8 @@
+import Character from "./character.js";
+
+const character = new Character(
+    "Dan"
+);
+
+console.log(character);
+console.log(character.name);
